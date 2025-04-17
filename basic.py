313e3ddd7a12777e82e5f66bcb907758d89ec5e5
ns-mkhalid <mkhalid@narrativescience.com>
2025-04-17 13:25:42 -0500

    updating basic

--- basic.py
+++ basic.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+restaurant_list = ["Chipotle", "Joys Noodles","Firefly Burgers"]
 
-restaurant_list = ["Chipotle"]
 
+@app.get("/restaurants")
+async def get_strings():
+    return {"restaurants": restaurant_list}
 
-@app.get("/restaurant")
-async def root():
-     return {"restaurants": restaurant_list}
+@app.post("/restaurants")
+async def add_string(name: str = ""):
+    restaurant_list.append(name)
+    return {"restaurants": restaurant_list}
+
+@app.delete("/restaurants")
+async def delete_string(index: int = 0):
+    restaurant_list.pop(index)
+    return {"restaurants": restaurant_list}
